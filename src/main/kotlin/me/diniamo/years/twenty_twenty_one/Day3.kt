b498e578d64016cab113c49d4de191c1b32932df
diniamo <denesrozsavolgyi@gmail.com>
2021-12-03 15:04:42 +0100

    day 3 part 2 + improve some stuff

--- src/main/kotlin/me/diniamo/years/twenty_twenty_one/Day3.kt
+++ src/main/kotlin/me/diniamo/years/twenty_twenty_one/Day3.kt
@@ -4,10 +4,22 @@ import me.diniamo.Day
 
 class Day3(year: Int) : Day<List<String>>(3, year) {
     override fun partOne(input: List<String>): Any {
+        val aInput = mutableListOf<String>()
+        val strBuilder = StringBuilder()
+
+        for(j in 0 until input[0].length) {
+            for(str in input) {
+                strBuilder.append(str[j])
+            }
+
+            aInput.add(strBuilder.toString())
+            strBuilder.clear()
+        }
+
         val gammaBuilder = StringBuilder()
         val epsilonBuilder = StringBuilder()
 
-        input.forEach {
+        aInput.forEach {
             val zeroCount = it.count { c -> c == '0' }
             val oneCount = it.count { c -> c == '1' }
 
@@ -20,26 +32,34 @@ class Day3(year: Int) : Day<List<String>>(3, year) {
             }
         }
 
-        return gammaBuilder.toString().toInt(2) * epsilonBuilder.toString().toInt(2)
+        return gammaBuilder.toString().toInt(2) * epsilonBuilder.toString().toInt(2)  // .toInt(2) -> convert binary string to decimal
     }
 
-    override fun partTwo(input: List<String>): Any {
-        return "TODO"
-    }
 
-    override fun adaptInput(input: List<String>): List<String> {
-        val newList = mutableListOf<String>()
-        val strBuilder = StringBuilder()
+    private fun partTwoRecursive(input: List<String>, index: Int, isLeast: Boolean): List<String> {
+        if(input.size == 1)
+            return input
 
-        for(j in 0 until input[0].length) {
-            for(str in input) {
-                strBuilder.append(str[j])
-            }
+        val oneCount = input.count { it[index] == '1' }
+        val zeroCount = input.count { it[index] == '0' }
 
-            newList.add(strBuilder.toString())
-            strBuilder.clear()
+        return if(oneCount > zeroCount || oneCount == zeroCount) {
+            partTwoRecursive(
+                if (isLeast) input.filter { it[index] == '0' } else input.filter { it[index] == '1' },
+                index + 1,
+                isLeast
+            )
+        } else {
+            partTwoRecursive(
+                if (isLeast) input.filter { it[index] == '1' } else input.filter { it[index] == '0' },
+                index + 1,
+                isLeast
+            )
         }
-
-        return newList.toList()
     }
+
+    override fun partTwo(input: List<String>): Any =
+        partTwoRecursive(input, 0, false)[0].toInt(2) * partTwoRecursive(input, 0, true)[0].toInt(2)
+
+    override fun adaptInput(input: List<String>): List<String> = input
 }
