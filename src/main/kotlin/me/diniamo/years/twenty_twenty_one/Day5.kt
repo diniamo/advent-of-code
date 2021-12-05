bff3787f4ff13282375cd913235005d317233bb6
diniamo <denesrozsavolgyi@gmail.com>
2021-12-05 19:17:41 +0100

    day 5 part 2 + debug stuff fully removed

--- src/main/kotlin/me/diniamo/years/twenty_twenty_one/Day5.kt
+++ src/main/kotlin/me/diniamo/years/twenty_twenty_one/Day5.kt
@@ -3,21 +3,53 @@ package me.diniamo.years.twenty_twenty_one
 import me.diniamo.Day
 import java.lang.Integer.max
 
+private enum class DiagDir(val x: Int, val y: Int) {
+    UP_LEFT(-1, -1),
+    UP_RIGHT(1, -1),
+    DOWN_LEFT(-1, 1),
+    DOWN_RIGHT(1, 1)
+}
+
 class Day5(year: Int) : Day<List<Pair<Pair<Int, Int>, Pair<Int, Int>>>>(5, year) {
-    private fun generateOcean(input: List<Pair<Pair<Int, Int>, Pair<Int, Int>>>): Array<Array<Int>> {
+    private fun generateOcean(input: List<Pair<Pair<Int, Int>, Pair<Int, Int>>>, diagonals: Boolean): Array<Array<Int>> {
         val maxX = max(input.maxOf { it.first.first }, input.maxOf { it.second.first }) + 1
         val maxY = max(input.maxOf { it.first.second }, input.maxOf { it.second.second }) + 1
 
         val ocean = Array(maxY) { Array(maxX) { 0 } }  // To access a point: ocean[y][x]
 
-        println(input)
         for(p in input) {
-            for(x in (p.first.first rangeOrDownTo p.second.first).also { println(it) }) {
-                for(y in (p.first.second rangeOrDownTo p.second.second).also { println(it) }) {
+            val isDiagonal = p.first.first != p.second.first && p.first.second != p.second.second
+            if(!diagonals && isDiagonal)
+                continue
+
+            if(isDiagonal) {
+                val x1 = p.first.first
+                val y1 = p.first.second
+                val x2 = p.second.first
+                val y2 = p.second.second
+
+                val dir: DiagDir = when {
+                    x1 > x2 && y1 > y2 -> DiagDir.UP_LEFT
+                    x1 < x2 && y1 > y2 -> DiagDir.UP_RIGHT
+                    x1 > x2 && y1 < y2 -> DiagDir.DOWN_LEFT
+                    x1 < x2 && y1 < y2 -> DiagDir.DOWN_RIGHT
+                    else -> throw IllegalStateException("not how diagonals works, just let me compile this")
+                }
+
+                var x = x1
+                var y = y1
+                while(x != x2 && y != y2) {
                     ocean[y][x]++
-                    println("$x, $y")
-                    ocean.forEach { println(it.contentToString().replace('0', '.')) }
-                    println()
+
+                    x += dir.x
+                    y += dir.y
+                }
+                ocean[y][x]++
+            } else {
+                for (x in (p.first.first rangeOrDownTo p.second.first)) {
+                    for (y in (p.first.second rangeOrDownTo p.second.second)) {
+                        ocean[y][x]++
+                    }
                 }
             }
         }
@@ -26,14 +58,13 @@ class Day5(year: Int) : Day<List<Pair<Pair<Int, Int>, Pair<Int, Int>>>>(5, year)
     }
 
     override fun partOne(input: List<Pair<Pair<Int, Int>, Pair<Int, Int>>>): Any {
-        val ocean = generateOcean(
-            input.filter { it.first.first == it.second.first || it.first.second == it.second.second }.also { println(it) }
-        )
+        val ocean = generateOcean(input, false)
         return ocean.sumOf { it.count { int -> int > 1 } }
     }
 
     override fun partTwo(input: List<Pair<Pair<Int, Int>, Pair<Int, Int>>>): Any {
-        return "TODO"
+        val ocean = generateOcean(input, true)
+        return ocean.sumOf { it.count { int -> int > 1 } }
     }
 
     override fun adaptInput(input: List<String>): List<Pair<Pair<Int, Int>, Pair<Int, Int>>> = input.map { str ->
