ba941022c4ed6a01c58b04a9d2a8e969f6e26ef2
diniamo <denesrozsavolgyi@gmail.com>
2021-12-07 07:50:57 +0100

    day 6 part 2

--- src/main/kotlin/me/diniamo/years/twenty_twenty_one/Day6.kt
+++ src/main/kotlin/me/diniamo/years/twenty_twenty_one/Day6.kt
@@ -3,22 +3,40 @@ package me.diniamo.years.twenty_twenty_one
 import me.diniamo.Day
 
 class Day6(year: Int) : Day<List<Int>>(6, year) {
-    private fun countLanternFish(start: List<Int>, amountOfDays: Int): Int {
-        val current = start.toMutableList()
+    private fun countLanternFish(start: List<Int>, amountOfDays: Int): Long {
+        val map = hashMapOf<Int, Long>(
+            0 to 0,
+            1 to 0,
+            2 to 0,
+            3 to 0,
+            4 to 0,
+            5 to 0,
+            6 to 0,
+            7 to 0,
+            8 to 0,
+        )
 
-        for(unused in 1..amountOfDays) {
-            current.toList().forEachIndexed { i, v ->
-                when(v) {
-                    0 -> {
-                        current[i] = 6
-                        current.add(8)
-                    }
-                    else -> current[i] -= 1
-                }
-            }
+        for(timer in start) {
+            map[timer] = map[timer]!!.plus(1)
         }
 
-        return current.size
+        for(day in 1..amountOfDays) {
+            val oz = map[0]!!
+
+            map[0] = map[1]!!
+            map[1] = map[2]!!
+            map[2] = map[3]!!
+            map[3] = map[4]!!
+            map[4] = map[5]!!
+            map[5] = map[6]!!
+            map[6] = map[7]!!
+            map[7] = map[8]!!
+
+            map[6] = map[6]!!.plus(oz)
+            map[8] = oz
+        }
+
+        return (0..8).sumOf { map[it]!! }
     }
 
     override fun partOne(input: List<Int>): Any = countLanternFish(input, 80)
